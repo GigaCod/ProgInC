1767e24652e1f0d22b60a599f4fdff04c7ddd7b8
Andrej Lich <a.lich@ostfalia.de>
2022-04-25 11:00:23 +0200

    Aufgabe 3

--- Aufgabe 3/Serie3.c
+++ Aufgabe 3/Serie3.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
+char *mystrstr(char *haystack, char *needle);
+
+
 int main(int argc,char *argv[])
 {
 //1----------------------------------------------
@@ -15,6 +18,7 @@ Wenn nichts gefunden wird wird null wiedergegeben.
 //2----------------------------------------------
 
 char src[]="hallo";
+char src2[]="abc_aaabd";
 char *ptr1=strstr(src,"ll"); // Erwarte: das er es erfolgreich findet.
 char *ptr2=strstr(src,"xy"); // Erwarte: NULL
 char *ptr3=strstr(src,"hallo123"); // Erwarte: Fehler
@@ -26,36 +30,77 @@ printf("%p %p \n",src,ptr2);
 printf("%p %p \n",src,ptr3);
 printf("%p %p \n",src,ptr4);
 printf("%p %p \n",src,ptr5);
+printf("%s\n",mystrstr(src2, "absd"));
 
+
+char str3[]="Hello World";
+char *ptr6=strtok(str3," ");
+char *ptr7=strtok(NULL," ");
+char *ptr8=strtok(NULL," ");
+char str4[]="hello,world ich";
+char *ptr9=strtok(str4," ,");  //Zweites Argument beinhaltet
+char *ptr10=strtok(NULL," ,");  //ein Leerzeichen und ein Komma
+char *ptr11=strtok(str4," ,");
+char *ptr12=strtok(NULL," ,");
+
+printf("%p %p \n",str3,ptr6);
+printf("%p %p \n",str3,ptr7);
+printf("%p %p \n",str3,ptr8);
+printf("%p %p \n",str4,ptr9);
+printf("%p %p \n",str4,ptr10);
+printf("%p %p \n",str4,ptr11);
+printf("%p %p \n",str4,ptr12);
 }
 
-//"abc_abcd1"  "abcd"
-// |
-// |
+//3------------------------------------------------
+
 char *mystrstr(char *haystack, char *needle) {
 
-    size_t haystackSize = strlen(haystack);
-    size_t needleSize = strlen(needle);
+    size_t haystackSize = strlen(haystack); // 5
+    size_t needleSize = strlen(needle); // 1
 
     if(haystackSize < needleSize){
-        return NULL;
+        return "NULL";
     }
     if(needleSize == 0){
         return haystack;
     }
-    int wordFound = 0;
 
+    int wordFound = 0;
     int needlePos = 0;
+    int tempHaystackPos = 0;
 
     for(int haystackPos = 0; haystackPos < haystackSize; haystackPos++){
-        for(int needlePos = 0; needlePos < needleSize; needlePos++ ){
-
-            if(haystack[haystackPos] == needle[needlePos]){
-                
+        printf("%i:%c vs %c \n",haystackPos, haystack[haystackPos], needle[needlePos]);
+        if(haystack[haystackPos] == needle[needlePos] && needleSize + haystackPos <= haystackSize){
+            tempHaystackPos = haystackPos;
+            for(int needleCounter = 0; needleCounter < needleSize; needleCounter++){
+                printf("In der Schleife \n");
+                if(haystack[tempHaystackPos] == needle[needlePos]){
+                    wordFound++;
+                    needlePos++;
+                    tempHaystackPos++;
+                    if(wordFound == needleSize){
+                        return &haystack[tempHaystackPos-needleSize];
+                    }
+                }else{
+                    wordFound = 0;
+                    needlePos = 0;
+                    needleCounter = needleSize;
+                }
             }
-        };
+        }else{
+            wordFound = 0;
+            needlePos = 0;
+        } 
 
     };
+    return "NULL";
 
 
 }
+//4------------------------------------------------
+
+
+
+
