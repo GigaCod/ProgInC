ecb5e4ff1337c17c9387ee26efb0b1f2b1a0711a
Andrej Lich <a.lich@ostfalia.de>
2022-05-01 23:24:20 +0200

    Made some changes

--- Aufgabe 3/Serie3.c
+++ Aufgabe 3/Serie3.c
@@ -24,13 +24,14 @@ char *ptr2=strstr(src,"xy"); // Erwarte: NULL
 char *ptr3=strstr(src,"hallo123"); // Erwarte: Fehler
 char *ptr4=strstr(src,"l"); // Erwarte: erstes Vorkomniss
 char *ptr5=strstr("ll",src); // Erwarte: Fehler 
-
+/*
 printf("%p %p \n",src,ptr1);
 printf("%p %p \n",src,ptr2);
 printf("%p %p \n",src,ptr3);
 printf("%p %p \n",src,ptr4);
 printf("%p %p \n",src,ptr5);
-printf("%s\n",mystrstr(src2, "absd"));
+*/
+printf("%p\n",mystrstr(src2, "absd"));
 
 
 char str3[]="Hello World";
@@ -38,13 +39,11 @@ char *ptr6=strtok(str3," ");
 char *ptr7=strtok(NULL," ");
 char *ptr8=strtok(NULL," ");
 char str4[]="hello,world ich";
-printf("%s \n",str4);
 char *ptr9=strtok(str4," ,");  //Zweites Argument beinhaltet
-printf("%s \n",str4);
 char *ptr10=strtok(NULL," ,");  //ein Leerzeichen und ein Komma
 char *ptr11=strtok(str4," ,");
 char *ptr12=strtok(NULL," ,");
-
+/*
 printf("%p %p \n",str3,ptr6);
 printf("%p %p \n",str3,ptr7);
 printf("%p %p \n",str3,ptr8);
@@ -52,9 +51,29 @@ printf("%p %p \n",str4,ptr9);
 printf("%p %p \n",str4,ptr10);
 printf("%p %p \n",str4,ptr11);
 printf("%p %p \n",str4,ptr12);
-
+*/
 char globalString[]="Ich bin ein sos String";
-printf("%s\n", mystrtok(globalString, " "));
+char globalString2[]="Ich bin ein sos String";
+
+printf("------------------ \n");
+printf("%s \n", mystrtok(globalString, " "));
+printf("%s \n", mystrtok(NULL, " "));
+printf("%s \n", mystrtok(NULL, " "));
+printf("%s \n", mystrtok(NULL, " "));
+printf("%s \n", mystrtok(NULL, " "));
+printf("%s \n", mystrtok(NULL, " "));
+printf("%s \n", mystrtok(NULL, " "));
+
+printf("------------------ \n");
+printf("%s \n", strtok(globalString2, " "));
+printf("%s \n", strtok(NULL, " "));
+printf("%s \n", strtok(NULL, " "));
+printf("%s \n", strtok(NULL, " "));
+printf("%s \n", strtok(NULL, " "));
+printf("%s \n", strtok(NULL, " "));
+printf("%s \n", strtok(NULL, " "));
+
+
 }
 
 //3------------------------------------------------
@@ -65,7 +84,7 @@ char *mystrstr(char *haystack, char *needle) {
     size_t needleSize = strlen(needle);
 
     if(haystackSize < needleSize){
-        return "NULL";
+        return NULL;
     }
     if(needleSize == 0){
         return haystack;
@@ -76,11 +95,9 @@ char *mystrstr(char *haystack, char *needle) {
     int tempHaystackPos = 0;
 
     for(int haystackPos = 0; haystackPos < haystackSize; haystackPos++){
-        printf("%i:%c vs %c \n",haystackPos, haystack[haystackPos], needle[needlePos]);
         if(haystack[haystackPos] == needle[needlePos] && needleSize + haystackPos <= haystackSize){
             tempHaystackPos = haystackPos;
             for(int needleCounter = 0; needleCounter < needleSize; needleCounter++){
-                printf("In der Schleife \n");
                 if(haystack[tempHaystackPos] == needle[needlePos]){
                     wordFound++;
                     needlePos++;
@@ -100,24 +117,56 @@ char *mystrstr(char *haystack, char *needle) {
         } 
 
     };
-    return "NULL";
+    return NULL;
 
 
 }
 //4------------------------------------------------
+
+int checkDelimiter(char character, const char *delim){
+    size_t delimSize = strlen(delim);
+
+    for(int i = 0; i < delimSize; i++){
+        //printf("Charakter: %c vs ",character);
+        //printf("Charakter: %c \n",delim[i]);
+        if(character == delim[i]){
+            //printf("ALARM \n");
+            return 1;
+        }
+    }
+    return 0;  
+}
 char *global = NULL;
 
 char *mystrtok(char *str, const char *delim){
 
+    size_t delimSize = strlen(delim);
+    int counter = 0;
+
     if( str != NULL){
         global = str;
-        for(int i = 0; global[i] != '\0'; i++) {
-
-        }
-        return NULL;
     }else{
-        for(int i = 0; global[i] != '\0'; i++) {
+        str = global;
+    }
 
+    while(!checkDelimiter(global[counter], delim) && global[counter] != '\0') {
+        counter++;
+    }
+
+    if(global[counter] != '\0'){
+        global[counter] = '\0';
+        counter++;
+        global = global + counter;
+        while(str[counter] != '\0') {
+            str[counter] = '\0';
+            counter++;
+        }
+        printf("String: %s \n",str);
+        return str;
+    }else{
+        global = global + counter;
+        if(strlen(str)){
+            return str;
         } 
     }
     return NULL;
